ab08785aada5f8638cd68eb9d13ab55d3e7e96a5
unknown <esgodoylumno.unlam.edu.ar>
2022-04-04 15:29:24 -0300

    Segundo commit agregue Switch

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -1,10 +1,54 @@
 package Calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+		Scanner teclado = new Scanner(System.in);
+		
+		Integer a =0;
+		Integer b = 0;
+		Integer opcion = 0;
+		Integer resultado = 0;
+		 do{
+		 System.out.println("Ingrese su operación \n 1 para sumar \n 2 para restar \n 3 para multiplicar \n 4 para dividir");
+		 opcion = teclado.nextInt();	 
+		 
+		 }while(!(opcion < 1 && opcion > 4));
+		 System.out.println("Ingrese el primer numero");
+		 a = teclado.nextInt();
+		 System.out.println("Ingrese el segundo numero");
+		 b = teclado.nextInt();
+		  switch(opcion)
+		  {
+		  case 1:
+			  resultado = a + b;
+		  break;
+		  case 2:
+			  resultado = a - b;
+			  break;
+		  case 3:
+			  resultado = a * b;
+			  break;
+		  case 4: 
+			  resultado = a / b;
+			  break;		  
+		  }
+		 
+		 /*if(opcion.equals(1)){
+		 resultado = a + b;
+		 }else if(opcion.equals(2)){
+		 resultado = a - b;
+		 }else if(opcion.equals(3)){
+		 resultado = a * b;
+		 }else{
+		 resultado = a / b;
+		 }*/
+		 System.out.println("El resultado es " + resultado.toString());
+		
 	}
 
 }
